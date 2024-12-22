0784deaafda08dd5f16141f9538a44ae00ac176d
Juana Betancourt <beta.juan.c@gmail.com>
2024-12-22 20:36:47 -0300

    cambio a Image en HeroSection

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,22 +1,25 @@
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
     <section style={{ textAlign: "center", padding: "20px" }}>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <img
+        <Image
           src="/foto_perfil.jpg"
           alt="Juan Betancourt"
+          width={150}
+          height={150}
           style={{
-            width: "150px",
-            height: "150px",
             borderRadius: "50%", // Esto hace que la imagen sea circular
             objectFit: "cover", // Asegura que la imagen se recorte correctamente
             objectPosition: "top", // Ajusta el enfoque hacia la parte superior de la imagen
             marginBottom: "20px",
           }}
+          priority // Indica que esta imagen es una prioridad para la carga
         />
       </div>
       <h1>Hola, soy Juan Betancourt</h1>
-      <p>Growth Marketer | Data Scientist | Innovador en IA</p>
+      <p>Growth Marketer | Data Scientist | Automatizador con IA</p>
     </section>
   );
 }
